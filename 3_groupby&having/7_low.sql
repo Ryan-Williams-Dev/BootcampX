1893e47a868db8afee9131862bbab0a8e7edb171
Ryan Williams <r.d.williams.ca@gmail.com>
2022-03-08 17:46:03 +0000

    new data

--- 3_groupby&having/7_low.sql
+++ 3_groupby&having/7_low.sql
@@ -1,8 +1,8 @@
 SELECT students.name AS student, avg(assignment_submissions.duration) AS average_assignment_duration, avg(assignments.duration) as average_estimated_duration
-FROM students
-JOIN assignment_submissions ON students.id = assignment_submissions.student_id
-JOIN assignments ON assignment_submissions.assignment_id = assignments.id
-WHERE students.end_date IS NULL
-GROUP BY student
-HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
-ORDER BY average_assignment_duration DESC;
+  FROM students
+  JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+  JOIN assignments ON assignment_submissions.assignment_id = assignments.id
+  WHERE students.end_date IS NULL
+  GROUP BY student
+  HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+  ORDER BY average_assignment_duration DESC;
